27563efeadec6b68834df1600a0bdb9ffb521299
pov <maosreypov6789@gmail.com>
2025-12-25 09:21:18 +0700

    test

---
+++ themes/my-theme/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>My theme</title>
+  <link rel="stylesheet" href="wp-content/themes/my-theme/style.css">
+</head>
+<body>
+
+<h1 class="h1">Hello POV </h1>
+<p>wwwwwwwwwww</p>
+
+
+</body>
+</html>
